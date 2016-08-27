52cf4a5d23c9b8f2d0c76808cfd27093fcacadbf
Rien Maertens <maertensrien@gmail.com>
2016-08-27 16:17:10 +0200

    improvements

--- Gemfile
+++ Gemfile
@@ -2,10 +2,6 @@ source "https://rubygems.org"
 
 # Nanoc is a static site generator
 gem 'nanoc'
-gem 'adsf'
-
-# Guard: compile when changes are detected
-gem 'guard-nanoc'
 
 # Write posts using markdown
 gem 'kramdown'
@@ -17,3 +13,12 @@ gem 'nokogiri'
 gem 'sass'
 gem 'bootstrap', '~> 4.0.0.alpha3.1'
 
+group :development do
+  # server
+  gem 'adsf'
+end
+
+group :nanoc do
+  # Guard: compile when changes are detected
+  gem 'guard-nanoc'
+end
---
+++ content/assets/js/freelancer.js
@@ -0,0 +1,37 @@
+/*!
+ * Start Bootstrap - Freelancer Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('body').on('click', '.page-scroll a, a.page-scroll', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 500);
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('a').click(function() {
+    $('.navbar-toggler:not(.collapsed)').click();
+});
+
+$(window).scroll(function() {
+   var hT = $('#kosmos').offset().top,
+       wH = $(window).height(),
+       wS = $(this).scrollTop();
+   if (wS + 40 > hT){
+      $('.navbar-fixed-top').addClass('navbar-shrink');
+   } else { 
+      $('.navbar-fixed-top').removeClass('navbar-shrink');
+   }
+});
